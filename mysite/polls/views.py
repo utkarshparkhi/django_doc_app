eea8968fb336c16be8d9628ca40af98c209467a0
utkarsh <uparkhi@ph.iitr.ac.in>
2019-05-20 17:23:20 +0530

    djangodoc app upto 4th tutorial

---
+++ mysite/polls/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse
+from .models import Question,Choice
+from django.template import loader
+# Create your views here.
+
+def index(request):
+    latest_questions = Question.objects.order_by('pub_date')
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_questions' :latest_questions
+    }
+    return render(request, 'polls/index.html',context)  
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/details.html', {'question': question})
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+    
